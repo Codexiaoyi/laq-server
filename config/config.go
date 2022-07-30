da052ae0fb893e22c4b625e225947717e04b122e
Codexiaoyi <874183200@qq.com>
2022-07-30 22:04:59 +0800

    [ADD]token编码

--- config/config.go
+++ config/config.go
@@ -1,7 +1,9 @@
 package config
 
 import (
+	"encoding/base64"
 	"fmt"
+	"log"
 
 	"gopkg.in/ini.v1"
 )
@@ -23,7 +25,11 @@ func init() {
 
 //加载数据资源配置
 func LoadSource(file *ini.File) {
-	AccessToken = file.Section("github").Key("AccessToken").MustString("")
+	decodeBytes, err := base64.StdEncoding.DecodeString(file.Section("github").Key("AccessToken").MustString(""))
+	if err != nil {
+		log.Fatalln(err)
+	}
+	AccessToken = string(decodeBytes)
 	Owner = file.Section("github").Key("Owner").MustString("")
 	Repo = file.Section("github").Key("Repo").MustString("")
 }
